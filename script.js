84ec0a8ee3cba42e440bec609815d8898145f103
Benji <asdf.tgz@gmail.com>
2020-08-09 00:24:34 +0200

    initial gh-pages commit

---
+++ script.js
@@ -0,0 +1,70 @@
+screw_pitch = 0.5;
+
+function raw(position) {
+  pos = position.toFixed(2);
+  return pos;
+}
+
+function degrees(position) {
+  deg = Math.round((position/screw_pitch*360));
+  deg = Math.abs(deg) + "°" + " " + ((deg > 0) ? 'CW' : 'CCW');
+  return deg;
+}
+
+// https://stackoverflow.com/a/23575406
+var gcd = function(a, b) {
+  if (b < 0.0000001) return a; // Since there is a limited precision we need to limit the value.
+  return gcd(b, Math.floor(a % b)); // Discard any fractions due to limitations in precision.
+};
+function fractions(position) {
+  abs = Math.abs(position/screw_pitch).toFixed(1);
+  var len = abs.toString().length - 2;
+  var denominator = Math.pow(10, len);
+  var numerator = abs * denominator;
+  var divisor = gcd(numerator, denominator);
+  numerator /= divisor;
+  denominator /= divisor;
+  
+  rat = Math.floor(numerator) + '/' + Math.floor(denominator);
+  rat = ((rat == "0/1") ? 0 : rat);
+  rat = rat + " " + ((position > 0) ? 'CW' : 'CCW');
+  return rat;
+}
+
+function convert() {
+  const array = ["top_left", "top_middle", "top_right", "middle_left", "middle_right", "bottom_left", "bottom_middle", "bottom_right"]
+  array.forEach(function (item) {
+    document.querySelector('#raw .' + item).innerHTML = raw(window[item]);
+    document.querySelector('#degrees .' + item).innerHTML = degrees(window[item]);
+    document.querySelector('#fractions .' + item).innerHTML = fractions(window[item]);
+  });
+}
+
+window.onload = function() {
+  document.getElementById("convert").onclick = function fun() {
+
+    var textarea = document.querySelector('textarea#input');
+    var textareaValue = textarea.value;
+    var arr = textareaValue.split(/\s+/).map(x=>+x);
+
+    center = (arr[7]+arr[8]+arr[13]+arr[13])/4;
+
+    top_left = arr[16] - center;
+    top_middle = ((arr[17]+arr[18])/2) - center;
+    top_right = arr[19] - center;
+    
+    middle_left = ((arr[6]+arr[11])/2) - center;
+    middle_right = ((arr[9]+arr[14])/2) - center;
+    
+    bottom_left = arr[1] - center;
+    bottom_middle = ((arr[2]+arr[3])/2) - center;
+    bottom_right = arr[4] - center;
+
+    convert()
+  }
+}
+
+//  1  2  3  4
+//  6  7  8  9
+// 11 12 13 14
+// 16 17 18 19
