a3ab732e76bdd592fc2b13edf33341941b796ff6
Benji <asdf.tgz@gmail.com>
2020-11-12 18:00:44 +0100

    UI revamp & some JS cleanup

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 screw_pitch = 0.5;
+cw = "CW&nbsp;&nbsp;⭮";
+ccw = "CCW ⭯";
 
 function raw(position) {
   pos = position.toFixed(2);
-  pos =  ((pos > 0) ? '+' : '') + pos
+  pos =  ((pos > 0) ? '+' : '') + pos + ' mm'
   return pos;
 }
 
 function degrees(position) {
   deg = Math.round((position/screw_pitch*360));
-  deg = Math.abs(deg) + '°' + ' ' + ((deg > 0) ? 'CW' : 'CCW');
-  return deg;
+  if (deg == 0) {
+    return deg + '° 🗸';
+  } else {
+    deg = Math.abs(deg) + '°' + ' ' + ((deg > 0) ? cw : ccw);
+    return deg;
+  }
 }
 
 // https://stackoverflow.com/a/23575406
@@ -28,12 +34,16 @@ function fractions(position) {
   
   rat = Math.floor(numerator) + '/' + Math.floor(denominator);
   rat = ((rat == '0/1') ? 0 : rat);
-  rat = rat + ' ' + ((position > 0) ? 'CW' : 'CCW');
-  return rat;
+  if (rat == 0) {
+    return rat + ' 🗸';
+  } else {
+    rat = rat + ' ' + ((position > 0) ? cw : ccw);
+    return rat;
+  }
 }
 
 function convert() {
-  const array = ['top_left', 'top_middle', 'top_right', 'middle_left', 'middle_right', 'bottom_left', 'bottom_middle', 'bottom_right']
+  const array = ['back_left', 'back_center', 'back_right', 'center_left', 'center_right', 'front_left', 'front_center', 'front_right']
   array.forEach(function (item) {
     document.querySelector('#raw .' + item).innerHTML = raw(window[item]);
     document.querySelector('#degrees .' + item).innerHTML = degrees(window[item]);
@@ -42,20 +52,18 @@ function convert() {
 }
 
 function plot(arr) {
-
   // Remove line numbers from array
   let forDeletion = [0, 5, 10, 15]
   for (var i = forDeletion.length -1; i >= 0; i--)
   arr.splice(forDeletion[i],1);
 
-  // Reverse the array because MBL output is backwards
+  // Reverse the array because MBL output is rotated
   arr = arr.reverse();
 
-  // Make array of arrays
   var arrs = [], size = 4;
   while (arr.length > 0)
   arrs.push(arr.splice(0, size));
-  
+
   // Plotly specific
   var data = [{
             z: arrs,
@@ -83,7 +91,6 @@ function plot(arr) {
     }
   };
   Plotly.newPlot('ploty', data, layout);
-  
 }
 
 function hide() {
@@ -100,24 +107,57 @@ window.onload = function() {
 
     center = (arr[7]+arr[8]+arr[12]+arr[13])/4;
 
-    top_left = arr[16] - center;
-    top_middle = ((arr[17]+arr[18])/2) - center;
-    top_right = arr[19] - center;
+    back_left = arr[16] - center;
+    back_center = ((arr[17]+arr[18])/2) - center;
+    back_right = arr[19] - center;
     
-    middle_left = ((arr[6]+arr[11])/2) - center;
-    middle_right = ((arr[9]+arr[14])/2) - center;
+    center_left = ((arr[6]+arr[11])/2) - center;
+    center_right = ((arr[9]+arr[14])/2) - center;
     
-    bottom_left = arr[1] - center;
-    bottom_middle = ((arr[2]+arr[3])/2) - center;
-    bottom_right = arr[4] - center;
+    front_left = arr[1] - center;
+    front_center = ((arr[2]+arr[3])/2) - center;
+    front_right = arr[4] - center;
 
     convert();
     plot(arr);
-    hide()
+    hide();
   }
+  popovers()
 }
 
 //  1  2  3  4
 //  6  7  8  9
 // 11 12 13 14
 // 16 17 18 19
+
+// Popovers
+function popovers() {
+  // Set popover data attributes
+  document.querySelectorAll('.back_left').forEach(x=>x.setAttribute('title', 'Back Left'))
+  document.querySelectorAll('.back_left').forEach(x=>x.setAttribute('data-content', "<img src='images/back-left.png' class='img-fluid'>"))
+  document.querySelectorAll('.back_center').forEach(x=>x.setAttribute('title', 'Back Center'))
+  document.querySelectorAll('.back_center').forEach(x=>x.setAttribute('data-content', "<img src='images/back-center.png' class='img-fluid'>"))
+  document.querySelectorAll('.back_right').forEach(x=>x.setAttribute('title', 'Back Right'))
+  document.querySelectorAll('.back_right').forEach(x=>x.setAttribute('data-content', "<img src='images/back-right.png' class='img-fluid'>"))
+  document.querySelectorAll('.center_left').forEach(x=>x.setAttribute('title', 'Center Left'))
+  document.querySelectorAll('.center_left').forEach(x=>x.setAttribute('data-content', "<img src='images/center-left.png' class='img-fluid'>"))
+  document.querySelectorAll('.center_center').forEach(x=>x.setAttribute('title', 'Center Center'))
+  document.querySelectorAll('.center_center').forEach(x=>x.setAttribute('data-content', "<img src='images/center-center.png' class='img-fluid'>"))
+  document.querySelectorAll('.center_right').forEach(x=>x.setAttribute('title', 'Center Right'))
+  document.querySelectorAll('.center_right').forEach(x=>x.setAttribute('data-content', "<img src='images/center-right.png' class='img-fluid'>"))
+  document.querySelectorAll('.front_left').forEach(x=>x.setAttribute('title', 'Front Left'))
+  document.querySelectorAll('.front_left').forEach(x=>x.setAttribute('data-content', "<img src='images/front-left.png' class='img-fluid'>"))
+  document.querySelectorAll('.front_center').forEach(x=>x.setAttribute('title', 'Front Center'))
+  document.querySelectorAll('.front_center').forEach(x=>x.setAttribute('data-content', "<img src='images/front-center.png' class='img-fluid'>"))
+  document.querySelectorAll('.front_right').forEach(x=>x.setAttribute('title', 'Front Right'))
+  document.querySelectorAll('.front_right').forEach(x=>x.setAttribute('data-content', "<img src='images/front-right.png' class='img-fluid'>"))
+  // Initiate popovers
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="popover"]'))
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl, {
+      html: true,
+      trigger: 'hover',
+      placement: 'left'
+    })
+  })
+}
