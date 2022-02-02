b55cc938471f4ff21dde53aa073785aec61bbbfa
Benji Korsak <asdf.tgz@gmail.com>
2022-02-02 16:08:54 +0100

    add easter egg

--- script.js
+++ script.js
@@ -74,6 +74,9 @@ function minMax(arr) {
 function maxDiff(arr) {
   let diff = Math.max(...arr) - Math.min(...arr);
   document.querySelector('#stats .max_diff').innerHTML = diff.toFixed(3);
+  if (diff <= 0.02) {
+    fireworks();
+  }
 }
 
 function avgDev(arr) {
@@ -117,6 +120,29 @@ function plot(arr) {
     }
   };
   Plotly.newPlot('ploty', data, layout);
+}
+
+function fireworks() {
+  var duration = 5 * 1000;
+  var animationEnd = Date.now() + duration;
+  var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+  
+  function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+  
+  var interval = setInterval(function() {
+    var timeLeft = animationEnd - Date.now();
+  
+    if (timeLeft <= 0) {
+      return clearInterval(interval);
+    }
+  
+    var particleCount = 50 * (timeLeft / duration);
+    // since particles fall down, start a bit higher than random
+    confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+    confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+  }, 250);
 }
 
 function hide() {
